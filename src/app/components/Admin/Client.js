9c6779580b45e0af14b53757ae8d9dbcd4185f72
kevinguzman0 <guzmankevin90@gmail.com>
2021-11-29 10:07:34 -0500

    ajuste client -5

--- src/app/components/Admin/Client.js
+++ src/app/components/Admin/Client.js
@@ -5,7 +5,7 @@ class Client extends React.Component {
     render() {
         return (
             <BoxPrincipal>
-                <iframe style={{ width: '100%', height: '100vh' }} src="https://sendmeproject.herokuapp.com/client" />
+                <iframe src="https://sendmeproject.herokuapp.com/client" />
             </BoxPrincipal>
         )
     }
